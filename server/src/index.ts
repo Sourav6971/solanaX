c95ccdd881ee820c82066d38da95e94f7577a160
sourav <mailsourav014@gmail.com>
2025-04-03 22:57:28 +0530

    initialized server

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app= express();
+
+
+
+app.get("/", (req: express.Request, res: express.Response): void => {
+    interface ResponseBody {
+        msg: string;
+    }
+
+    const responseBody: ResponseBody = {
+        msg: "App running successfully"
+    };
+
+    res.json(responseBody);
+});
